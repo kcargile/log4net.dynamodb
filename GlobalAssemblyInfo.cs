04eea26f2990f4ed9d474f4ebba79d7114b5f2f3
mohsen.k <mohsen.k@TGG-SYD-PC240>
2016-08-12 10:41:49 +1000

    Changing the assembly details to have a aws2 prefix so it can be uploaded to nuget.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
--- log4net.dynamodb/Properties/AssemblyInfo.cs
+++ log4net.dynamodb/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("log4net.dynamodb")]
+[assembly: AssemblyTitle("log4net.dynamodb.aws2")]
 [assembly: AssemblyDescription("log4net appender for Amazon Web Services (AWS) DynamoDb databases.")]
 [assembly: AssemblyCompany("Kristopher Cargile")]
-[assembly: AssemblyProduct("log4net.dynamodb")]
+[assembly: AssemblyProduct("log4net.dynamodb.aws2")]
 [assembly: AssemblyCopyright("Copyright © 2013 Cargile Technology Group, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("c4d10242-8ed6-48c5-9467-3fbd8767ec43")]
+[assembly: Guid("081fa355-eda0-4b34-bba6-134d87b2417e")]
